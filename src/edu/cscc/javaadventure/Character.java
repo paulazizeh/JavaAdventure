3e47731508f5eb252e091ea631a1961c36f01fec
Jim Kirkbride <jkirkbride@cscc.edu>
2020-04-25 17:19:57 -0400

    Basic Character class implemented.

---
+++ src/edu/cscc/javaadventure/Character.java
@@ -0,0 +1,122 @@
+package edu.cscc.javaadventure;
+
+public class Character {
+
+    private String name;
+    private String description;
+    private Integer age;
+    private String gender;
+    private Integer strength;
+    private Integer dexterity;
+    private Integer constitution;
+    private Integer intelligence;
+    private Integer wisdom;
+    private Integer charisma;
+    private Integer health;
+
+    public Character() {
+    }
+
+    public Character(String name, String description, Integer age, String gender, Integer strength, Integer dexterity,
+                     Integer constitution, Integer intelligence, Integer wisdom, Integer charisma, Integer health) {
+        this.name = name;
+        this.description = description;
+        this.age = age;
+        this.gender = gender;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.constitution = constitution;
+        this.intelligence = intelligence;
+        this.wisdom = wisdom;
+        this.charisma = charisma;
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getStrength() {
+        return strength;
+    }
+
+    public void setStrength(Integer strength) {
+        this.strength = strength;
+    }
+
+    public Integer getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(Integer dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public Integer getConstitution() {
+        return constitution;
+    }
+
+    public void setConstitution(Integer constitution) {
+        this.constitution = constitution;
+    }
+
+    public Integer getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(Integer intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public Integer getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(Integer wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public Integer getCharisma() {
+        return charisma;
+    }
+
+    public void setCharisma(Integer charisma) {
+        this.charisma = charisma;
+    }
+
+    public Integer getHealth() {
+        return health;
+    }
+
+    public void setHealth(Integer health) {
+        this.health = health;
+    }
+}
