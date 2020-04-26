411af1b8fbb4d8bcb1ce9e4f505bb1710d4ef703
Jim Kirkbride <jkirkbride@cscc.edu>
2020-04-26 13:55:34 -0400

    Adding code for Lab 2 starting point.

---
+++ src/com/cscc/javaadventure/Lantern.java
@@ -0,0 +1,85 @@
+package com.cscc.javaadventure;
+
+/**
+ * Represents a lantern in a role-playing game.
+ * The lantern can be lit or extinguished, broken or fixed.
+ * It also has a weight and a description.
+ */
+public class Lantern {
+    private boolean lit;
+    private double weight;
+    private boolean broken;
+
+    /**
+     * Constructs the lantern.
+     * By default the lantern is unlit, unbroken, and has a weight of 1.50.
+     */
+    public Lantern() {
+        weight = 1.50;
+    }
+
+    /**
+     * Indicates of the lantern is lit or not.
+     * @return true if lit, false otherwise.
+     */
+    public boolean isLit() {
+        return lit;
+    }
+
+    /**
+     * Lights the lantern.
+     */
+    public void light() {
+        this.lit = true;
+    }
+
+    /**
+     * Extinguishes the lantern.
+     */
+    public void extinguish() {
+        this.lit = false;
+    }
+
+    /**
+     * Get the description of the lantern. Indicates of the lantern is lit or not.
+     * @return The lantern description depending on if it is lit or not.
+     */
+    public String getDescription() {
+        return "A tarnished, old lantern that has seen better days. " + getLitDescription();
+    }
+
+    private String getLitDescription() {
+        return !lit ? "It is unlit." : "It glows softly.";
+    }
+
+    /**
+     * Gets the lantern's weight.
+     * @return The weight of the lantern to a precision of 2 decimal places.
+     */
+    public double getWeight() {
+        return weight;
+    }
+
+    /**
+     * Indicates of the lantern is broken or not.
+     * @return true if the lantern is broken, false otherwise.
+     */
+    public boolean isBroken() {
+        return broken;
+    }
+
+    /**
+     * Repairs the lantern.
+     */
+    public void fix() {
+        this.broken = false;
+    }
+
+    /**
+     * Breaks the lantern.
+     * NOTE: "break" is a keyword in Java, so we can't call this method "break".
+     */
+    public void makeBroken() {
+        this.broken = true;
+    }
+}
---
+++ src/com/cscc/javaadventure/TreasureChest.java
@@ -0,0 +1,98 @@
+package com.cscc.javaadventure;
+
+/**
+ * Represents a treasure chest in a role-playing game.
+ * The treasure chst can be locked or unlocked, and open or unopened.
+ * A locked chest must be unlocked before it can be opened. Locking a chest
+ * also closes it.
+ * A treasure chest also has a weight and a description. The description is
+ * different depending on if the chest is locked or not.
+ */
+public class TreasureChest {
+    private double weight;
+    private boolean open;
+    private boolean locked;
+
+    /**
+     * Constructs a TreasureChest. A treasure chest has a default weight
+     * and is locked and closed by default.
+     */
+    public TreasureChest() {
+        this.weight = 10.00;
+        this.locked = true;
+    }
+
+    /**
+     * Returns the weight of the treasure chest.
+     * @return The weight of the treasure chest to a precision of two decimal places.
+     */
+    public double getWeight() {
+        return weight;
+    }
+
+    /**
+     * Indicates if the treasure chest is open or not.
+     * @return true if it is open (which also implies it is unlocked), false otherwise.
+     */
+    public boolean isOpen() {
+        return open;
+    }
+
+    /**
+     * Indicates if the treasure chest is locked or not.
+     * @return true if the chest is locked, false otherwise.
+     */
+    public boolean isLocked() {
+        return locked;
+    }
+
+    /**
+     * Locks the chest. It also closes the chest if it was open.
+     */
+    public void lock() {
+        this.locked = true;
+        this.close();
+    }
+
+    /**
+     * Unlocks the chest.
+     * Note, this method does *not* open the chest.
+     */
+    public void unlock() {
+        this.locked = false;
+    }
+
+    /**
+     * Opens the chest if it is unlocked.
+     * @return true if the chest is unlocked, false otherwise.
+     */
+    public boolean open() {
+        if (this.locked) {
+            return false;
+        }
+
+        this.open = true;
+
+        return this.open;
+    }
+
+    /**
+     * Closes the chest.
+     * Notes: This method does *not* also lock the chest.
+     */
+    public void close() {
+        this.open = false;
+    }
+
+    /**
+     * Retrieves the description of the chest.
+     * @return The description of the chest, which is based on whether it is locked or not.
+     */
+    public String getDescription() {
+        return "A sturdy iron chest. " + getLockDescription();
+    }
+
+    private String getLockDescription() {
+        return this.locked ? "It is locked." : "It is unlocked.";
+    }
+}
