e6229b9c91d576431804c3f00d27d7b45f3957a9
Jim Kirkbride <jkirkbride@cscc.edu>
2020-04-26 08:29:06 -0400

    Adding example code for debugging.

--- src/edu/cscc/javaadventure/Main.java
+++ src/edu/cscc/javaadventure/Main.java
@@ -3,5 +3,18 @@ package edu.cscc.javaadventure;
 public class Main {
     public static void main(String[] args) {
         System.out.println("The adventure begins!");
+
+        Character character = new Character();
+        character.setName("Gandalf");
+        character.setDescription("The Gray Wizard");
+        character.setHealth(100);
+
+        String name = character.getName();
+        String description = character.getDescription();
+        Integer health = character.getHealth();
+
+        System.out.println(name);
+        System.out.println(description);
+        System.out.println(health);
     }
 }
