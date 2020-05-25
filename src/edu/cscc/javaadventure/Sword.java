1f902acf6bf1480a05b5e18c206bb99d9b18df9b
Jim Kirkbride <jkirkbride@cscc.edu>
2020-05-25 18:45:27 -0400

    Adding Sword.

---
+++ src/edu/cscc/javaadventure/Sword.java
@@ -0,0 +1,29 @@
+package edu.cscc.javaadventure;
+
+public class Sword extends JAObject {
+
+    private Integer damage;
+    private Boolean twoHanded;
+
+    Sword() {
+        super("Sword", "A razor sharp broadsword forged from dark steel", 8.0);
+        this.damage = 10;
+        this.twoHanded = true;
+    }
+
+    public Integer getDamage() {
+        return damage;
+    }
+
+    public void setDamage(Integer damage) {
+        this.damage = damage;
+    }
+
+    public Boolean getTwoHanded() {
+        return twoHanded;
+    }
+
+    public void setTwoHanded(Boolean twoHanded) {
+        this.twoHanded = twoHanded;
+    }
+}
