4fb491998348f1a3e7c2a4a7e39c6846edb14b2a
Jim Kirkbride <jkirkbride@cscc.edu>
2020-05-31 16:31:17 -0400

    Adding character selection.

--- src/edu/cscc/javaadventure/Main.java
+++ src/edu/cscc/javaadventure/Main.java
@@ -1,6 +1,8 @@
 package edu.cscc.javaadventure;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,11 +11,39 @@ public class Main {
         Character wizard = new Character("Gandalf");
         Character fighter = new Character("Gimli");
         Character ringBearer = new Character("Frodo");
-
         Party party = new Party();
-        party.addMember(wizard);
-        party.addMember(fighter);
-        party.addMember(ringBearer);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean done = false;
+        do {
+            System.out.println("Choose your party members.");
+            System.out.println("Who would you like to add?");
+            System.out.println("1. " + wizard.getName());
+            System.out.println("2. " + fighter.getName());
+            System.out.println("3. " + ringBearer.getName());
+            System.out.println("4. Done");
+            System.out.print(" (Choices are 1, 2, 3, 4): ");
+            String choice = scanner.nextLine();
+            switch(choice) {
+                case "1":
+                    party.addMember(wizard);
+                    break;
+                case "2":
+                    party.addMember(fighter);
+                    break;
+                case "3":
+                    party.addMember(ringBearer);
+                    break;
+                case "4":
+                    done = true;
+                    break;
+                default:
+                    done = true;
+                    party.addMember(wizard);
+                    party.addMember(fighter);
+                    party.addMember(ringBearer);
+            }
+        } while(!done);
 
         List<Character> members = party.getMembers();
         System.out.println("Party size: " + party.size());
