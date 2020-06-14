41475aa750e9c7c7ddfb9fa06ee2e19bcd40e33e
Jim Kirkbride <jkirkbride@cscc.edu>
2020-06-14 16:25:59 -0400

    Updating the docs and libs.

--- src/edu/cscc/javaadventure/AddPartyMemberEventHandler.java
+++
@@ -1,5 +0,0 @@
-package edu.cscc.javaadventure;
-
-public interface AddPartyMemberEventHandler {
-    void onEvent(Character memberAdded);
-}
--- src/edu/cscc/javaadventure/RemovePartyMemberEventHandler.java
+++
@@ -1,5 +0,0 @@
-package edu.cscc.javaadventure;
-
-public interface RemovePartyMemberEventHandler {
-    void onEvent(Character member);
-}
